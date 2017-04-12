d2eaa1716bc045eb23f836b864afe5470cc09c27
Russell McClellan <russell.mcclellan@gmail.com>
2017-04-11 22:34:53 -0400

    publish sierra bottle

--- Formula/armv4t-none-eabi-binutils.rb
+++ Formula/armv4t-none-eabi-binutils.rb
@@ -4,6 +4,11 @@ class Armv4tNoneEabiBinutils < Formula
   url "https://ftp.gnu.org/gnu/binutils/binutils-2.28.tar.gz"
   sha256 "cd717966fc761d840d451dbd58d44e1e5b92949d2073d75b73fccb476d772fcf"
 
+  bottle do
+    root_url "https://github.com/russellmcc/homebrew-armv4t-toolchain/releases/download/2.28/"
+    sha256 "dc14531a163738a3f754db42f18103e59e682961c803d4ceecf565436d935c14" => :sierra
+  end
+
   def install
     # Remove unrecognized options if warned by configure
     system "./configure", "--disable-debug",
